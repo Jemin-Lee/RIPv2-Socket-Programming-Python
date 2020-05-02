d19c1553dbc665dee051d8e84a0a1b6ffb0551fd
Robin Ji <xji47@uclive.ac.nz>
2020-05-02 21:43:30 +1200

    Upload New File

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gnome-terminal -- python3 router.py config_1.ini
+gnome-terminal -- python3 router.py config_2.ini
+gnome-terminal -- python3 router.py config_3.ini
+gnome-terminal -- python3 router.py config_4.ini
+gnome-terminal -- python3 router.py config_5.ini
+gnome-terminal -- python3 router.py config_6.ini
+gnome-terminal -- python3 router.py config_7.ini
